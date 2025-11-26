8989eb258a9e57b8fbe66dc9d561690020f0fa53
Net Nguyen <netnguyen270@gmail.com>
2025-11-26 23:06:37 +0700

    TC LoginTest

--- src/main/java/org/example/LoginTest.java
+++ src/main/java/org/example/LoginTest.java
@@ -3,22 +3,20 @@ package org.example;
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
-
+import org.junit.Before;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.file.Paths;
 import java.time.Duration;
 
-public class FirstTestAndroidAppiumTestNG {
+public class LoginTest {
   private AndroidDriver driver;
   private WebDriverWait wait;
 
@@ -27,7 +25,7 @@ public class FirstTestAndroidAppiumTestNG {
   private static final String APP_ACTIVITY = "com.saucelabs.mydemoapp.android.view.activities.MainActivity";
   private static final String APPIUM_SERVER_URL = "http://127.0.0.1:4723";
 
-  @BeforeMethod
+  @BeforeClass
   public void setUp() throws MalformedURLException {
     // Configure UiAutomator2 options for Android
     String appPath = Paths
@@ -37,7 +35,6 @@ public class FirstTestAndroidAppiumTestNG {
 
     UiAutomator2Options options = new UiAutomator2Options()
         .setPlatformName("Android")
-        .setPlatformVersion("11")
         .setAutomationName("UiAutomator2")
         .setDeviceName("Android Emulator")
         .setApp(appPath)
@@ -53,19 +50,40 @@ public class FirstTestAndroidAppiumTestNG {
 
     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     System.out.println("Test setup completed successfully");
+
+    driver.findElement(By.id("com.saucelabs.mydemoapp.android:id/menuIV")).click();
+    driver.findElement(AppiumBy.accessibilityId("Login Menu Item")).click();
   }
 
-  @Test(priority = 1, description = "Verify app launches successfully")
-  public void testAppLaunch() {
-    System.out.println("Starting testAppLaunch...");
+  @Test()
+  public void TC_01_LoginWithEmptyData(){
+    driver.findElement(By.id("com.saucelabs.mydemoapp.android:id/nameET")).sendKeys("");
+    driver.findElement(By.id("com.saucelabs.mydemoapp.android:id/passwordET")).sendKeys("");
+    driver.findElement(By.id("com.saucelabs.mydemoapp.android:id/loginBtn")).click();
 
-    WebElement appLogoAndName = wait.until(ExpectedConditions.visibilityOfElementLocated(
-        AppiumBy.accessibilityId("App logo and name")));
-    Assert.assertTrue(appLogoAndName.isDisplayed(), "App Logo should be visible");
-    System.out.println("App launched successfully - App Logo is visible");
+    //verify
+    Assert.assertEquals(driver.findElement(By.id("com.saucelabs.mydemoapp.android:id/nameErrorTV")).getText(), "Username is required");
   }
 
-  @AfterMethod
+  @Test()
+  public void TC_02_LoginSuccess() {
+    driver.findElement(By.id("com.saucelabs.mydemoapp.android:id/nameET")).sendKeys("bob@example.com");
+    driver.findElement(By.id("com.saucelabs.mydemoapp.android:id/passwordET")).sendKeys("10203040");
+    driver.findElement(By.id("com.saucelabs.mydemoapp.android:id/loginBtn")).click();
+
+    System.out.println("Login thành công!");
+  }
+
+  @Test()
+  public void TC_03_Logout() throws InterruptedException {
+    Thread.sleep(400);
+    driver.findElement(By.id("com.saucelabs.mydemoapp.android:id/menuIV")).click();
+    driver.findElement(AppiumBy.accessibilityId("Logout Menu Item")).click();
+
+    System.out.println("Logout thành công!");
+  }
+
+  @AfterClass
   public void tearDown() {
     if (driver != null) {
       try {
